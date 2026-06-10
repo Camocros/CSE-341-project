39ac38c578224687394ec74d26f0d7b3ce369f46
camocros <sernaarciniegas@gmail.com>
2026-06-09 18:43:37 -0600

    fix: use correct contact fields for creating and updating

--- controllers/users.js
+++ controllers/users.js
@@ -20,10 +20,11 @@ const getSingle = async (req, res) => {
 
 const createUser = async (req, res) => {
   const user = {
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
     email: req.body.email,
-    username: req.body.username,
-    name: req.body.name,
-    ipaddress: req.body.ipaddress
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
   };
 
   const response = await mongodb.getDatabase().db().collection('contacts').insertOne(user);
@@ -38,10 +39,11 @@ const createUser = async (req, res) => {
 const updateUser = async (req, res) => {
   const userId = new ObjectId(req.params.id);
   const user = {
-    username: req.body.username,
+    firstName: req.body.firstName,
+    lastName: req.body.lastName,
     email: req.body.email,
-    name: req.body.name,
-    ipaddress: req.body.ipaddress
+    favoriteColor: req.body.favoriteColor,
+    birthday: req.body.birthday
   };
 
   const response = await mongodb.getDatabase().db().collection('contacts').replaceOne({ _id: userId }, user);
--- swagger.js
+++ swagger.js
@@ -5,6 +5,8 @@ const doc = {
     title: 'Contacts API',
     description: 'Contacts API for CSE 341'
   },
+  // TODO: Replace with your actual Render URL, for example: 'my-project.onrender.com'
+  // Or, you can use process.env.HOST if you have it set in your .env file
   host: 'localhost:3001',
   schemes: ['http', 'https']
 };
